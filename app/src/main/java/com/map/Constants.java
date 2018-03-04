36b0591f6bf877d40a7f8ea2bceb9b7102a2a836
hata02525 <hata02525@gmail.com>
2018-03-04 22:30:03 +0530

    Google Map Polyline pikar place search

---
+++ app/src/main/java/com/map/Constants.java
@@ -0,0 +1,11 @@
+package com.map;
+
+
+public class Constants {
+
+   public static int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+   public static int Pickup_REQUEST_CODE = 232;
+   public static int Drop_REQUEST_CODE = 233;
+   public static int is_check = 0;
+
+}
